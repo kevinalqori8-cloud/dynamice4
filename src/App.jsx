9bf7176576353d8ded59f31ee913d3c8c9512f81
kevinalqori8-cloud <kevinalqori8@gmail.com>
2025-12-01 06:41:37 +0700

    checking bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 // src/App.jsx
 import React, { useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import Carousel from "./Pages/Gallery";
 import FullWidthTabs from "./Pages/Tabs";
 import Footer from "./Pages/Footer";
-import ChatAnonimModal from "./components/ChatAnonimModal"; // modal kecil
-import ChatButton from "./components/ChatButton"; // tombol trigger
+import Chat from "./components/ChatAnonim"; // pakai versi lama tanpa modal
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-/* ---------- LAYOUT UTAMA (akan selalu ditampilkan) ---------- */
-function Layout() {
-	useEffect(() => AOS.init({ duration: 800, once: true }), []);
+function App() {
+	useEffect(() => {
+		AOS.init({ duration: 800, once: true });
+	}, []);
+
 	return (
 		<>
 			<Home />
 			<Carousel />
 			<FullWidthTabs />
 
-			{/* tombol trigger chat */}
-			<div className="hidden lg:flex justify-center mt-[-3rem] mb-20">
-				<ChatButton />
+			{/* Chat versi lama (bukan modal) */}
+			<div
+				className="lg:mx-[12%] lg:mt-[-5rem] lg:mb-20 hidden lg:block"
+				id="ChatAnonim_lg"
+				data-aos="fade-up"
+				data-aos-duration="1200"
+			>
+				<Chat />
 			</div>
-			<div className="lg:hidden fixed bottom-6 left-1/2 -translate-x-1/2 z-40">
-				<ChatButton className="shadow-lg" />
+
+			{/* Mobile: tampilkan juga Chat (bukan modal) */}
+			<div className="lg:hidden px-4 py-6">
+				<Chat />
 			</div>
 
 			<div id="Mesh1"></div>
@@ -33,21 +40,5 @@ function Layout() {
 	);
 }
 
-/* ---------- APP ---------- */
-export default function App() {
-	return (
-		<>
-			{/* 1. selalu tampilkan Layout (Home, Gallery, Tabs, Footer) */}
-			<Layout />
-
-			{/* 2. modal chat (hanya muncul saat URL = /chat) */}
-			<Routes>
-				<Route path="/chat" element={<ChatAnonimModal />} />
-			</Routes>
-
-			{/* 3. portal untuk modal (render di luar #root) */}
-			<div id="chat-portal" />
-		</>
-	);
-}
+export default App;
 
