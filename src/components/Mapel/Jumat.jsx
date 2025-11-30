4665bed91f6495a489a9051df89d5897b14353a7
kevinalqori8-cloud <kevinalqori8@gmail.com>
2025-12-01 03:50:13 +0700

    Initial Commit

---
+++ src/components/Mapel/Jumat.jsx
@@ -0,0 +1,43 @@
+const Jumat = () => {
+	return (
+		<>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">KKA</div>
+				<div className="flex justify-center items-center text-sm">2 Jam</div>
+			</div>
+			<div
+				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Sejarah</div>
+				<div className="flex justify-center items-center text-sm">2 Jam</div>
+			</div>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="700">
+				<div className="w-[50%] text-base  font-medium">Fisika</div>
+				<div className="flex justify-center items-center text-sm">1 Jam</div>
+			</div>
+			<div
+				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Biologi</div>
+				<div className="flex justify-center items-center text-sm">1 Jam</div>
+			</div>
+			<div
+				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Geografi</div>
+				<div className="flex justify-center items-center text-sm">2 Jam</div>
+			</div>
+		</>
+	)
+}
+
+export default Jumat
---
+++ src/components/Mapel/Senin.jsx
@@ -0,0 +1,40 @@
+const Senin = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Biologi</div>
+					<div className="flex justify-center items-center text-sm">2 Jam
+					</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Sosiologi</div>
+					<div className="flex justify-center items-center text-sm">2 Jam
+					</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Agama</div>
+					<div className="flex justify-center items-center text-sm">2 Jam</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+					<div className="flex justify-center items-center text-sm">1 Jam</div>
+				</div>
+
+				
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
+					<div className="flex justify-center items-center text-sm">1 Jam</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Penjas</div>
+					<div className="flex justify-center items-center text-sm">1 Jam</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Sejarah</div>
+					<div className="flex justify-center items-center text-sm">1 Jam</div>
+				</div>
+        </>
+    )
+} 
+
+export default Senin;
