fafbcb90759407b0c7b40a5b63db06a015b44b5f
kevinalqori8-cloud <kevinalqori8@gmail.com>
2025-12-01 04:05:25 +0700

    Fix error

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -33,7 +33,7 @@ const Kamis = () => {
 				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium"Seni Musik</div>
+				<div className="w-[50%] text-base  font-medium">Seni Musik</div>
 				<div className="flex justify-center items-center text-sm">2 Jam</div>
 			</div>
 
